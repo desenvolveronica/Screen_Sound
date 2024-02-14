21db3f54334efc0dad7b2e6a063d8ab8c5945c45
Verônica Helena <desenvolveronica@gmail.com>
2024-02-14 10:36:14 -0300

    Condicional com switch case

--- ProjetoConsole/Program.cs
+++ ProjetoConsole/Program.cs
@@ -22,6 +22,30 @@ void ExibirMenu()
     Console.WriteLine("Digite 3 para avaliar uma banda");
     Console.WriteLine("Digite 4 para exibir a média uma banda");
     Console.WriteLine("Digite -1 para sair");
+
+    Console.Write("\nDigite sua opção: ");
+    string opcao = Console.ReadLine()!;
+    
+    
+    int opcaoEscolhida = int.Parse(opcao);
+    switch (opcaoEscolhida)//em parênteses coloca quem é o alvo da verificação
+    {
+        case 1: Console.WriteLine(" Você escolheu a opção " + opcaoEscolhida);
+            break;
+        case 2: Console.WriteLine(" Você escolheu a opção " + opcaoEscolhida);
+            break;
+        case 3:
+            Console.WriteLine(" Você escolheu a opção " + opcaoEscolhida);
+            break;
+        case 4:
+            Console.WriteLine(" Você escolheu a opção " + opcaoEscolhida);
+            break;
+        case -1:
+            Console.WriteLine(" Você escolheu a opção " + opcaoEscolhida);
+            break;
+        default: Console.WriteLine("Opção Inválida");
+            break;
+    }
 }
 ExibirMensagemBoasVindas();
 ExibirMenu();
