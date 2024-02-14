8b687e3f9bdd27c0016872fb115090265f6b2570
Verônica Helena <desenvolveronica@gmail.com>
2024-02-14 09:47:34 -0300

    Ajustes

--- ProjetoConsole/Program.cs
+++ ProjetoConsole/Program.cs
@@ -15,4 +15,13 @@ void ExibirMensagemBoasVindas()
     Console.WriteLine(mensagem);
 }
 
+void ExibirMenu()
+{
+    Console.WriteLine("\nDigite 1 para registrar uma banda");
+    Console.WriteLine("Digite 2 para mostrar todas as bandas");
+    Console.WriteLine("Digite 3 para avaliar uma banda");
+    Console.WriteLine("Digite 4 para exibir a média uma banda");
+    Console.WriteLine("Digite -1 para sair");
+}
 ExibirMensagemBoasVindas();
+ExibirMenu();
