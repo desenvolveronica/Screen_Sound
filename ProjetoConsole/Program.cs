9f58b5014c94715d2b10be40347968e0ec7832f0
Verônica Helena <desenvolveronica@gmail.com>
2024-02-17 14:42:53 -0300

    Exibindo lista e index

--- ProjetoConsole/Program.cs
+++ ProjetoConsole/Program.cs
@@ -39,7 +39,7 @@ void ExibirMenu()
                 RegistrarBanda();
                 break;
             case 2:
-                Console.WriteLine(" Você escolheu a opção " + opcaoEscolhida);
+                MostrarBandasRegistradas();
                 break;
             case 3:
                 Console.WriteLine(" Você escolheu a opção " + opcaoEscolhida);
@@ -67,14 +67,44 @@ void ExibirMenu()
 void RegistrarBanda()
 {
     Console.Clear();
-    Console.WriteLine("Registro de Bandas\n");
+    Console.WriteLine("*********************");
+    Console.WriteLine("Registro de Bandas");
+    Console.WriteLine("*********************\n");
+
     Console.Write("Digite o nome da banda que você quer registrar: ");
     string nomeDaBanda = Console.ReadLine()!;//colocando o ! indicamos que não queremos trabalhar com valor nulo
     ListaDeBandas.Add(nomeDaBanda);
-    Console.WriteLine(ListaDeBandas.Count);
     Console.WriteLine($"A banda {nomeDaBanda} foi registrada com sucesso!");
     Thread.Sleep(2000);
     Console.Clear();
     ExibirMenu();
 }
+void MostrarBandasRegistradas()
+{
+    Console.Clear();
+    Console.WriteLine("*********************");
+    Console.WriteLine("Bandas Registradas");
+    Console.WriteLine("*********************\n");
+    if (ListaDeBandas.Count == 0)
+    {
+        Console.WriteLine("Esta Lista está vazia");
+    }
+    else
+    {
+        //percorrer em um for para exibir todas as bandas da lista
+        var posicao = 1;
+        for (int i = 0; i < ListaDeBandas.Count; i++)
+        {
+            var itensLista = ListaDeBandas[i].ToString();
+            var posItem = posicao++;
+            Console.WriteLine($"{posItem} - {itensLista}");
+        }
+    }
+    
+    Console.WriteLine("\nPressione qualquer tecla para voltar ao menu principal");
+    Console.ReadKey();
+    Console.Clear();
+    ExibirMenu();
+}
+
 ExibirMenu();
